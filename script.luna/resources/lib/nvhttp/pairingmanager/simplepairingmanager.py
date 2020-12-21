e0dc6fb7b438c68d2e8d579372d5f67592c5a5c1
stickman89 <csstikman89@aol.com>
2020-12-21 02:04:25 +0000

    Use expect_unbuffer for pairing process to avoid buffered output.

--- script.luna/resources/lib/nvhttp/pairingmanager/simplepairingmanager.py
+++ script.luna/resources/lib/nvhttp/pairingmanager/simplepairingmanager.py
@@ -16,7 +16,7 @@ class SimplePairingManager(AbstractPairingManager):
 
     def pair(self, nvhttp, server_info, dialog):
         self.logger.info('[MoonlightHelper] - Attempting to pair host: ' + self.config_helper.host_ip)
-        pairing_proc = subprocess.Popen(["moonlight", "pair"], cwd="/storage/moonlight", env={'LD_LIBRARY_PATH': '/storage/moonlight'}, shell=False, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+        pairing_proc = subprocess.Popen(["expect_unbuffer", "moonlight", "pair"], cwd="/storage/moonlight", env={'LD_LIBRARY_PATH': '/storage/moonlight'}, shell=False, stdout=subprocess.PIPE, preexec_fn=os.setsid)
         lines_iterator = iter(pairing_proc.stdout.readline, b"")
 
         pairing_thread = threading.Thread(target=self.loop_lines, args=(self.logger, lines_iterator, dialog))
