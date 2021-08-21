1e6bf4c3478b279c78b0d56d7b040757b1473a5d
TheChoconut <thechoconut@outlook.com>
2021-08-21 20:35:37 +0200

    Fix a bug where all usb devices would be reset.

--- resources/launchscripts/coreelec/postscript.sh
+++ resources/launchscripts/coreelec/postscript.sh
@@ -4,3 +4,4 @@ echo 0 > /sys/class/graphics/fb0/blank
 echo 0 > /sys/class/graphics/fb1/blank
 echo "rm default" > /sys/class/vfm/map
 echo "add default decoder ppmgr amlvideo deinterlace amvideo" > /sys/class/vfm/map
+python reset_hid.py
--- resources/launchscripts/coreelec/reset_hid.py
+++ resources/launchscripts/coreelec/reset_hid.py
@@ -8,8 +8,8 @@ for a in devices:
         if os.path.isfile(ifacePath):
             with open(ifacePath, 'r') as f:
                 interface = int(f.read())
-        if interface == 3 and os.path.isfile(base + a + '/authorized'):
-            os.system('echo 0 > ' + base + a + '/authorized')
-            os.system('echo 1 > ' + base + a + '/authorized')
+            if interface == 3 and os.path.isfile(base + a + '/authorized'):
+                os.system('echo 0 > ' + base + a + '/authorized')
+                os.system('echo 1 > ' + base + a + '/authorized')
     except:
         continue
